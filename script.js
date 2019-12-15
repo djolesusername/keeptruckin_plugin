67208405315030ccb5e60213701194859e07b973
Djordje Cvetkovic <djole001@gmail.com>
2019-12-15 17:55:42 +0100

    Add files via upload
    Added button

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 
-window.addEventListener("click", clicked)
-
+// window.addEventListener("click", clicked)
 
   
 function clicked(click, click2, click3) {
-   
-   //1.Copy current location to clipboard
-    let cityname = click.path[1].nextElementSibling.children[1].innerHTML.trim();
+//1. Copy City name to clipboard
+
+  let cityname = click.path[1].innerText.trim();
 
     navigator.clipboard.writeText(cityname).then(function () {
 
         //2. Open Google maps in new tab using coordinats 
-        let coordinats = click.path[1].nextElementSibling.children[2].innerHTML.toString().trim().split(",").join("+")
+        let coordinats = click.path[2].childNodes[8].innerText.toString().trim().split(",").join("+")
         let baseurl = "https://www.google.com/maps/dir/"
         let fulllink = baseurl.concat(coordinats)
         let win = window.open(fulllink, "_blank");
@@ -20,13 +19,29 @@ function clicked(click, click2, click3) {
                 
               }, function() {
         console.log("fail")
-      /* clipboard write failed */
+      // clipboard write failed 
     });
 
-
-
-
-
+  
   }
 
+
  
+  //KT fires onload event before vehicles info is recieved. I am using settimeout to wait for data from KT
+  //Adding buttons to the page once data is recieved
+ 
+window.onload = setTimeout (function () {
+   let djole6 = document.querySelectorAll("span.location-coords.text")
+
+djole6.forEach( 
+    function(currentValue, currentIndex, listObj) { 
+        var button = document.createElement("input");
+        button.type= "button";
+        button.addEventListener("click", clicked);
+        button.setAttribute("type", "button");
+        button.setAttribute("value", "maps");
+currentValue.append(button)
+    
+} )
+ }, 3000) 
+
