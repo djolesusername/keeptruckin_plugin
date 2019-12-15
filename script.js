4d96a8b0d4e8742577709d64d9e855938f6a7e81
Djordje Cvetkovic <djole001@gmail.com>
2019-12-15 21:48:57 +0100

    Add files via upload
    added regex to vehicle page

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
 
-// window.addEventListener("click", clicked)
+ //window.addEventListener("click", clicked)
 
   
 function clicked(click, click2, click3) {
-//1. Copy City name to clipboard
+//1. Copy City name to clipboard. 
+//Regex will make sure that city name gets copied to clipboard (regardless of how user toggles between coordinates and city nam in kt)
 
-  let cityname = click.path[1].innerText.trim();
-
+  let city = click.path[1].innerText
+  let cityname = city.toString().trim() 
+  let regx = /^\d{2}[.]/
+  if (cityname.match(regx)) {
+    cityname = click.path[1].previousElementSibling.innerText.trim();
+  }
+  
     navigator.clipboard.writeText(cityname).then(function () {
 
         //2. Open Google maps in new tab using coordinats 
-        let coordinats = click.path[2].childNodes[8].innerText.toString().trim().split(",").join("+")
+        let coordinats = click.path[2].childNodes[8].innerText //|| click.path[1].innerText 
+        let cleaned = coordinats.toString().trim().split(",").join("+")
         let baseurl = "https://www.google.com/maps/dir/"
-        let fulllink = baseurl.concat(coordinats)
+        let fulllink = baseurl.concat(cleaned)
         let win = window.open(fulllink, "_blank");
         win.focus();
         
@@ -22,8 +29,8 @@ function clicked(click, click2, click3) {
       // clipboard write failed 
     });
 
+}
   
-  }
 
 
  
@@ -40,6 +47,9 @@ djole6.forEach(
         button.addEventListener("click", clicked);
         button.setAttribute("type", "button");
         button.setAttribute("value", "maps");
+    
+   
+
 currentValue.append(button)
     
 } )
